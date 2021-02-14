6e0266e6e86543d6cb90a12a89be136e94b0a0f8
gustavo-bueno <gustavobcarvalho22@gmail.com>
2021-02-13 23:32:34 -0300

    feat: presentationScreen added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import * as Font from 'expo-font';
 
-import Routes from './src/routes'
+import Routes from './src/routes';
 
-export default function App() {
-  return (
-    <Routes />
-  );
-}
+const fonts = {
+  'Ubuntu': require('./assets/fonts/Ubuntu-Regular.ttf'),
+  'Ubuntu-Bold': require('./assets/fonts/Ubuntu-Bold.ttf'),
+};
 
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
 
+  const loadFonts = async () => {
+    await Font.loadAsync(fonts);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  if (loading) return <View />;
+  return <Routes />;
+};
+
+export default App;
